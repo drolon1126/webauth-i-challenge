bc6db1edd3d186f559b16e5a2537141eb3ae2f91
danRolon <daniel.rolon@yahoo.com>
2019-10-14 15:40:40 -0500

    endpoints for get /users, post /login and /register completed

--- server.js
+++ server.js
@@ -13,20 +13,48 @@ server.use(express.json());
 server.use(cors());
 
 server.post('/api/register', (req, res) => {
+  let user = req.body;
 
+  if (user.username && user.password) {
+    user.password = bcrypt.hashSync(user.password, 14);
+
+    Users.add(user)
+      .then(saved => {
+        res.status(201).json(saved);
+      })
+      .catch(error => {
+        res.status(500).json({message:'There was a problem adding user'});
+      });
+  } else {
+    res.status(400).json({ message: 'Please submit a username and password' });
+  }
 });
 
 server.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+  if (username && password) {
+    Users.findByUN(username)
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+      } else {
+        res.status(401).json({ message: 'Invalid username or password' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({message:'There was a problem getting user'});
+    });
+  }
 
 });
 
-server.get('/api/users',(req,res)=>{
+server.get('/api/users', (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => {
-      res.status(500).json({message:'There was a problem getting users'});
+      res.status(500).json({ message: 'There was a problem getting users' });
     })
 });
 
