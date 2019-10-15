ffa21e47511c0d7eabe197b0688760a9a031f403
danRolon <daniel.rolon@yahoo.com>
2019-10-15 18:05:10 -0500

    added session cookies to check if logged in, also added a logout endpoint. moves user endpoint to its own router. started working on a restricted router that would use the restricted middleware globally.

--- server.js
+++ server.js
@@ -2,14 +2,40 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const sessions = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(sessions);
 
 const Users = require('./users/user-model.js');
+const userRouter = require('./users/user-router.js');
+const knexConfig = require('./data/dbConfig.js');
 
 const server = express();
 
+const sessionConfiguration = {
+  name: 'ohfosho',
+  secret: 'keep it secret, keep it safe!', 
+  cookie: {
+    httpOnly: true, 
+    maxAge: 1000 * 60 * 60,
+    secure: false, 
+  },
+  resave: false,
+  saveUninitialized: true, 
+
+  // change to use our database instead of memory to save the sessions
+  store: new KnexSessionStore({
+    knex: knexConfig,
+    createtable: true, // automatically create the sessions table
+    clearInterval: 1000 * 60 * 30, // delete expired sessions every 30
+  }),
+};
+
+server.use(sessions(sessionConfiguration));
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/api/users', userRouter);
 
 server.post('/api/register', (req, res) => {
   let user = req.body;
@@ -30,14 +56,15 @@ server.post('/api/register', (req, res) => {
 });
 
 server.post('/api/login', (req, res) => {
-  const { username, password } = req.body;
+  let { username, password } = req.body;
   if (username && password) {
     Users.findByUN(username)
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+          req.session.username = user.username;
           res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
-        res.status(401).json({ message: 'Invalid username or password' });
+        res.status(401).json({ message: 'You shall not pass!' });
       }
     })
     .catch(error => {
@@ -47,20 +74,14 @@ server.post('/api/login', (req, res) => {
 
 });
 
-server.get('/api/users', restricted, (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'There was a problem getting users' });
-    })
+server.get('/api/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      res.status(200).json({message:'you have been logged out'});
+    });
+  } else {
+    res.status(200).json({ message: 'already logged out' });
+  }
 });
-
-function restricted(req,res,next){
-  //move this to global for stretch.
-  //write login validation here
-  res.status(401).json({ message: 'You cannot pass!!' });
-}
 
 module.exports = server;
